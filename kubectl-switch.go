cee5be7b3a0827f28bfd8553b5780adf7c1f334a
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2019-06-12 20:47:51 +0100

    Make the cmd line less noisy make version read from file

--- kubectl-switch.go
+++ kubectl-switch.go
@@ -25,6 +25,41 @@ func seedData() Configuration {
 	return data
 }
 
+func initialize() Configuration {
+	// check if ~/.kube/kubectl exists if not create it
+	// fmt.Println("checking ~/.kube/kubectl")
+	home, err := createKubectlHome()
+	if err != nil {
+		panic(err)
+	}
+	// if env var does not exists try to read from ~/.kube/kubectl/config
+	config := fmt.Sprintf("%v/config", home)
+	if _, err := os.Stat(config); os.IsNotExist(err) {
+		_, err := os.Create(config)
+		if err != nil {
+			panic(err)
+		}
+		seed, seedErr := json.MarshalIndent(seedData(), "", " ")
+		if seedErr != nil {
+			panic(seedErr)
+		}
+		// fmt.Println("writing file ~/.kube/kubectl/config")
+		noWriteErr := ioutil.WriteFile(config, seed, 0666)
+		if noWriteErr != nil {
+			panic(noWriteErr)
+		}
+	}
+	// fmt.Println("File ~/.kube/kubectl/config exists now reading it....")
+	// config file is already exists at this point (created above or already exists) read it now
+	configFile, err := ioutil.ReadFile(config)
+	data := Configuration{}
+	jsonErr := json.Unmarshal([]byte(configFile), &data)
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+	return data
+}
+
 // Get prefix url downloading the binary
 // Most of the time it will be "https://storage.googleapis.com/kubernetes-release/release"
 // A file will be created in ~/.kube/kubectl/config if you do not have this env var KUBECTL_PREFIX set
@@ -33,56 +68,24 @@ func prefix() string {
 	//  check env var KUBECTL_PREFIX
 	prefix := os.Getenv("KUBECTL_PREFIX")
 	if prefix == "" {
-		// check if ~/.kube/kubectl exists if not create it
-		fmt.Println("creating ~/.kube/kubectl")
-		home, err := createKubectlHome()
-		if err != nil {
-			panic(err)
-		}
-		// if env var does not exists try to read from ~/.kube/kubectl/config
-		config := fmt.Sprintf("%v/config", home)
-		if _, err := os.Stat(config); os.IsNotExist(err) {
-			_, err := os.Create(config)
-			if err != nil {
-				panic(err)
-			}
-			seed, seedErr := json.MarshalIndent(seedData(), "", " ")
-			if seedErr != nil {
-				panic(seedErr)
-			}
-			fmt.Println("writing file ~/.kube/kubectl/config")
-			noWriteErr := ioutil.WriteFile(config, seed, 0666)
-			if noWriteErr != nil {
-				panic(noWriteErr)
-			}
-		}
-		fmt.Println("File ~/.kube/kubectl/config exists now reading it....")
-		// config file is already exists at this point (created above or already exists) read it now
-		configFile, err := ioutil.ReadFile(config)
-		data := Configuration{}
-		jsonErr := json.Unmarshal([]byte(configFile), &data)
-		if jsonErr != nil {
-			panic(jsonErr)
-		}
-		fmt.Println("Read URL Prefix: ", data.KubectlPrefix)
-		fmt.Println("Read version: ", data.KubectlVersion)
+		data := initialize()
 		return data.KubectlPrefix
 	}
-	return "https://storage.googleapis.com/kubernetes-release/release"
+	return prefix
 }
 
 // This function returns a string of the version
 func version() string {
-	fmt.Println(os.Args)
-	if len(os.Args) > 1 {
-		versionArgs := os.Args[1]
-		return versionArgs // uses cmd line arguments
+	version := os.Getenv("KUBECTL_VERSION")
+	if version == "" {
+		data := initialize()
+		return data.KubectlVersion
 	}
 	// TODO need to add input checker here
 	// TODO get the stable version here https://storage.googleapis.com/kubernetes-release/release/stable.txt
 	// TODO get the latest version here https://storage.googleapis.com/kubernetes-release/release/latest.txt
 	// TODO return stable version by default
-	return "v1.14.3"
+	return version
 }
 
 // Uses version() and home string to return the fullpath of the kubectl ex: ~/.kube/kubectl/kubectl-v1.14.3
@@ -109,7 +112,6 @@ func createKubectlHome() (string, error) {
 // Builds the url where to download the binary from returns this url as string
 // for example https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
 func buildURL() string {
-	fmt.Printf("%v/%v/bin/%v/%v/kubectl", prefix(), version(), runtime.GOOS, runtime.GOARCH)
 	return fmt.Sprintf("%v/%v/bin/%v/%v/kubectl", prefix(), version(), runtime.GOOS, runtime.GOARCH)
 }
 
@@ -157,7 +159,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("\n ====== downloading kubectl ver: %v from %v ...", version(), buildURL())
+	fmt.Printf("\n #### downloading kubectl ver: %v from %v ###", version(), buildURL())
 	if err := downloadFile(versionFile(home), buildURL()); err != nil {
 		panic(err)
 	}
@@ -165,17 +167,17 @@ func main() {
 	if errMod != nil {
 		panic(errMod)
 	}
-
-	fmt.Println("\nexport PATH=~/.kube/kubectl:$PATH")
-	fmt.Printf("Use kubectl-%v for execution", version())
+	fmt.Println("\n##### Export the new path below")
+	fmt.Println("\nexport PATH=~/.kube/kubectl:$PATH\n")
 	errSoftlink := softlinkKubectl(versionFile(home), kubectlFile(home))
 	if errSoftlink != nil {
 		panic(errSoftlink)
 	}
 	// TODO: make this into a cobra cmd line
-	// TODO: Make version() read from the json file
 	// TODO: Annotate this using godoc
 	// TODO: create a list commmand to show which versions are available on the machine
 	// TODO: create a list command to remotely show which versions are available
+	// TODO: create log-levels for debugging and warning
+	// TODO: create a progress bar
 	// Sample structure {"versions" : ["v1.14.3","v1.14.0"], "version_active": "v1.14.3","url_prefix": ...}
 }
