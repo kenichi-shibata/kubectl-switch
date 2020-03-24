8fc63a6471f05ae56d770c12d25d25eb8ca0d7a7
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2020-03-24 17:47:05 +0000

    Finished testing put back the main func

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import "github.com/kenichi-shibata/kubectl-switch/utils"
+import "github.com/kenichi-shibata/kubectl-switch/cmd"
 
 // Version change this to use go semver
 var Version = "v0.0.2"
 
 func main() {
-	// cmd.Version = Version
-	// cmd.Execute()
-	utils.LatestVer()
+	cmd.Version = Version
+	cmd.Execute()
 }
