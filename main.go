5b744d98c83855be765a94b2da91181deeb33722
Kenichi Shibata <kenichi.shibata@bglgroup.co.uk>
2020-03-24 18:22:48 +0000

    Fixed the version parser

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/kenichi-shibata/kubectl-switch/cmd"
+import (
+	"github.com/kenichi-shibata/kubectl-switch/utils"
+)
 
 // Version change this to use go semver
 var Version = "v0.0.2"
 
 func main() {
-	cmd.Version = Version
-	cmd.Execute()
+	// cmd.Version = Version
+	// cmd.Execute()
+	// fmt.Println("underconst")
+	utils.ParseKubectlVersion("v1.16.0-alpha.3")
 }
